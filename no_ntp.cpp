a12a0f60be7052c5ecddac97fb54d60c4407fb21
gregorthebigmac <gregorthebigmac@gmail.com>
2019-05-22 11:12:26 -0500

    Appears to be finding the hostname, time to test on the VMs

--- no_ntp.cpp
+++ no_ntp.cpp
@@ -7,7 +7,7 @@ using std::vector;
 
 void error_dump(string failed_command, vector<string> error_dump);
 string purge_newline(string hostname);
-int get_priority(vector<string> terminal_feedback);
+int get_priority(vector<string> terminal_feedback, string hostname);
 
 int main() {
 	command cmd;
@@ -26,7 +26,7 @@ int main() {
 	terminal_feedback.clear();
 	cmd.exec(cmd_get_priority.c_str(), terminal_feedback, error_list);
 	if (error_list.size() < 1) {
-		my_priority = get_priority(terminal_feedback); }
+		my_priority = get_priority(terminal_feedback, hostname); }
 	else error_dump(cmd_get_priority, error_list);
 
 	return 0;
@@ -51,11 +51,23 @@ string purge_newline(string hostname) {
 	return temp;
 }
 
-int get_priority(vector<string> terminal_feedback) {
+int get_priority(vector<string> terminal_feedback, string hostname) {
 	cout << "Loading priority config file..." << endl;
+	string str_priority = "";
 	for (int i = 0; i < terminal_feedback.size(); i++) {
-		cout << terminal_feedback[i];
+		if (terminal_feedback[i][0] != '#') {
+			string temp = terminal_feedback[i];
+			std::size_t found_host = temp.find(hostname);
+			if (found_host != std::string::npos) {
+				std::size_t found_priority = temp.find("=");
+				if (found_priority != std::string::npos) {
+					str_priority = temp.substr(0,found_priority);
+					break;
+				}
+			}
+		}
 	}
 	cout << "[done]" << endl << endl;
+	cout << "Found " << hostname << " priority: " << str_priority << endl << endl;
 	return 0;
 }
